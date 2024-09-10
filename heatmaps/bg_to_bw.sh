96351e3bbfd7710e60ac0941bdfe7d69a73e0f25
maddylee13 <162836230+maddylee13@users.noreply.github.com>
2024-09-10 14:24:42 +0100

    Update bg_to_bw.sh

--- heatmaps/bg_to_bw.sh
+++ heatmaps/bg_to_bw.sh
@@ -11,9 +11,9 @@ tid=$PBS_ARRAY_INDEX
 module load anaconda3/personal
 source activate ucsc-tools
 
-DIR=/rds/general/user/mml120/ephemeral/p2/CR_000/alignment/bedgraph/normalized
-OUTDIR=/rds/general/user/mml120/ephemeral/p2/CR_000/peakCalling/normalized/bigwig
-chromSizes=/rds/general/project/traditiom/live/epimem/genome_indexes/chm13v2.0/chm13v2.0.chrom.sizes
+DIR=/path/to/home/directory/alignment/bedgraph/normalized
+OUTDIR=/path/to/home/directory/peakCalling/normalized/bigwig
+chromSizes=/path/to/genome/directory/genome_indexes/chm13v2.0/chm13v2.0.chrom.sizes
 
 files=$(ls $DIR/*_bowtie2.fragments.normalized.bedgraph)
 arr=($files)
